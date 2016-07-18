a75214373fa1747f17787de814089327b46665c7
Johann Kerbrat <johann@kerbrat.eu>
2016-07-17 22:52:47 -0700

    step 1 - setting up the project

---
+++ js/app.js
@@ -0,0 +1,1 @@
+console.log('Holberton school workshop');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./js/app.js",
+    output: {
+        path: __dirname,
+        filename: "./static/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
