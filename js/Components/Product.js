b1ab9c93e898fe8872a4f78f01282991b3806674
Johann Kerbrat <johann@kerbrat.eu>
2016-07-19 18:12:09 -0700

    step 4 - styling

--- js/Components/Product.js
+++ js/Components/Product.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
+const borderColor = '#e6e3e3';
+const styles = {
+  item : {
+    borderRadius: '3px',
+    border: '1px solid ' + borderColor,
+    marginLeft: '10px',
+    padding: '0px 10px 0px 10px',
+  },
+  title: {
+    textDecoration: 'underline',
+  },
+  button: {
+    background: '#00a0d8',
+    color: 'white',
+    borderRadius: '5px',
+    textTransform: 'uppercase',
+    padding: '10px',
+    textAlign: 'center',
+  }
+};
+
 const Product = (props) => (
-  <div>
-    <h3>{props.name}</h3>
+  <div style={styles.item}>
+    <h3 style={styles.title}>{props.name}</h3>
     <img src={props.imageUrl} width={120} />
     <p>Quantity available: {props.quantityAvailable}</p>
     <p>Price: {props.price}</p>
-    <p>Buy one</p>
+    <p style={styles.button}>Buy one</p>
   </div>
 );
 
