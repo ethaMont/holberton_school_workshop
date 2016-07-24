2f17be13ed217efee705111114c32efdebc2f899
Johann Kerbrat <johann@kerbrat.eu>
2016-07-23 17:53:03 -0700

    step 10: provider

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ProductsList from './Components/ProductsList.js';
-import Product from './Components/Product.js';
+import ProductsList from './Components/ConnectProductsList.js';
+import ProductsReducer from './Reducers/Products.js';
 
-ReactDOM.render(
-  (<ProductsList>
-    <Product
-      imageUrl={"https://drscdn.500px.org/photo/161924045/q%3D80_m%3D2000/32f7531f9bf5468535171a86067d3218"}
-      name={"My book on React"} />
-    <Product
-      name={"My second book on React"} />
-    <Product
-      name={"My third book on React"} />
-  </ProductsList>),
-  document.getElementById('product')
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+} from 'redux';
+
+import {
+  Provider,
+} from 'react-redux';
+
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  combineReducers({ ProductsReducer }),
+  applyMiddleware(thunk)
 );
+
+ReactDOM.render((
+  <Provider store={store}>
+    <ProductsList />
+  </Provider>
+), document.getElementById('product'));
